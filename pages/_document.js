46891a6fa6c5fcd7cbbbb135e38365d96803cc4c
Andrew Romans <andrewromans@Andrews-MacBook-Pro.local>
2022-01-24 16:28:44 -0500

    Something weird 32

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,4 @@
-import { ColorModeScript } from "@chakra-ui/react"
-import { ColorModeProvider, ThemeProvider } from "@chakra-ui/react"
 import NextDocument, { Html, Head, Main, NextScript } from "next/document"
-import theme from "../lib/theme"
 
 export default class Document extends NextDocument {
   render() {
@@ -9,12 +6,8 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head />
         <body>
-          {/* 👇 Here's the script */}
-          <ThemeProvider theme={theme}>
-                <ColorModeProvider options={{useSystsemColorMode: true}} />
-                <Main />
-                <NextScript />
-          </ThemeProvider>
+          <Main />
+          <NextScript />
         </body>
       </Html>
     )
